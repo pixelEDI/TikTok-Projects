7cc4f9bfd740e3169617de53b060b759f4911e29
pixelEDI <96004062+pixelEDI@users.noreply.github.com>
2022-10-26 13:45:53 +0200

    Update main.cpp

--- 12_DigitalLaserMeasuringDevice/src/main.cpp
+++ 12_DigitalLaserMeasuringDevice/src/main.cpp
@@ -1,3 +1,20 @@
+/*
+
+
+        _          _ ______ _____ _____ 
+       (_)        | |  ____|  __ \_   _|
+  _ __  ___  _____| | |__  | |  | || |  
+ | '_ \| \ \/ / _ \ |  __| | |  | || |  
+ | |_) | |>  <  __/ | |____| |__| || |_ 
+ | .__/|_/_/\_\___|_|______|_____/_____|
+ | |                                    
+ |_|                                    
+                             
+www.pixeledi.eu | https://linktr.ee/pixeledi
+Digital Measuring Device | V1.0 | 10.2022
+
+*/
+
 #include <Arduino.h>
 #include <Wire.h>             // for the ToF Sensor
 #include <VL53L0X.h>          // for the ToF Sensor
