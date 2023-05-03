dd56e970778d88d40dbe245233c274a78a2a9a17
pixeledi <hello@pixeledi.eu>
2023-05-03 08:47:46 +0200

    added max7219 panel

---
+++ 19_MAX7219Panel/readme.md
@@ -0,0 +1,12 @@
+# General info
+
+Easy Text example of an MAX7219 Panel
+
+## Hardware
+- ESP32
+- MAX7219 Panel
+
+
+<img src="https://github.com/pixelEDI/TikTok-Projects/blob/641a6f16354f3849f408de90f6f50fb591a00046/16_I2S_InternetRadio/inetradio_Steckplatine.jpg" width="500">
+
+You like it? Then I would be very happy if you treat me to a coffee on [ko-fi.com/pixeledi](https://www.ko-fi.com/pixeledi)
---
+++ 19_MAX7219Panel/src/main.cpp
@@ -0,0 +1,59 @@
+/*
+        _          _ ______ _____ _____ 
+       (_)        | |  ____|  __ \_   _|
+  _ __  ___  _____| | |__  | |  | || |  
+ | '_ \| \ \/ / _ \ |  __| | |  | || |  
+ | |_) | |>  <  __/ | |____| |__| || |_ 
+ | .__/|_/_/\_\___|_|______|_____/_____|
+ | |                                    
+ |_|                                    
+                             
+www.pixeledi.eu | https://linktr.ee/pixeledi
+MAX72xx Panel | V1.0 | 05.2023
+*/
+#include <Arduino.h>
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+#include <Wire.h>
+
+unsigned long previousMillis = millis();
+
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 4
+// all other Pins are SPI Pins from ESP32
+#define CS_PIN 20
+MD_Parola mx = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// other Variants
+// https://majicdesigns.github.io/MD_Parola/class_m_d___parola.html
+
+#define BUF_SIZE 75
+char pxMessage[BUF_SIZE] = {"pixelEDI"};
+uint16_t eco2, etvoc, errstat, raw;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  mx.begin();
+  mx.setIntensity(2);
+  mx.displayClear();
+  mx.displayScroll(pxMessage, PA_RIGHT, PA_SCROLL_LEFT, 60);
+
+  Wire.begin();
+
+  Serial.println("Setup finished");
+}
+
+void loop()
+{
+
+  // first example
+  // mx.setTextAlignment(PA_LEFT);
+  // mx.print("ESP32");
+
+  if (mx.displayAnimate())
+  {
+    mx.displayReset();
+  }
+}
